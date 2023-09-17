36b2ec6ea8182f148b42231110de091654703eba
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-17 21:37:50 +0600

    Movies - React project

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,13 @@
+import { Movie } from "./Movie";
+
 function Movies(props) {
+    const { movies } = props;
+
+    return <div className="movies">
+        {movies.map(movie => {
+            return <Movie key={movie.imdbID} {...movie} />
+        })}
+    </div>
 
 }
 
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,30 @@
-function Main() {
-    return <main className="container content">
-        Some Text
-    </main>
+import React, { Component } from "react";
+import { Movies } from "../components/Movies";
+import { Preloader } from "../components/Preloader";
+
+class Main extends Component {
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=244d78e9&s=matrix')
+            .then(response => response.json())
+            .then(data => this.setState({ movies: data.Search }))
+    }
+
+    render() {
+        const { movies } = this.state
+
+        return <main className="container content">
+            {
+                movies.length ? (<Movies movies={this.state.movies} />) : <Preloader />
+            }
+
+        </main>
+    }
+
+
 }
 
 export { Main }
